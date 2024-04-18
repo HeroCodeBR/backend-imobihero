bdf71f3729eae7a3544a404a819b9166a28c7f0e
Alexia Kattah <alexiakattah@gmail.com>
2024-04-18 19:10:13 -0300

    feat: aula 12

---
+++ users/src/infra/repositories/users.repository.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+import { prisma } from '../prisma';
+
+class UsersRepositoryPrisma implements UsersRepository {
+  prisma: PrismaClient;
+  constructor() {
+    this.prisma = prisma;
+  }
+  async getUsers(): Promise<any> {
+    return await this.prisma.user.findMany();
+  }
+  createUser(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export { UsersRepositoryPrisma };
