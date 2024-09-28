0eceeccc9f80a8795ba0fe6e6f17f009f488ddec
Alexia Kattah <alexiakattah@Alexias-MacBook-Pro.local>
2024-09-28 17:51:47 -0300

    aula 19 - docker

--- users/dist/app/repositories/permission.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PermissionRepository = void 0;
-class PermissionRepository {
-}
-exports.PermissionRepository = PermissionRepository;
